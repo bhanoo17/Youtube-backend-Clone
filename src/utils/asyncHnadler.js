af51a8a67823b29d2557da593abc6edab42d4482
Bhaanoo Vishwakarma <bhanoolohar007@gmail.com>
2024-03-11 20:04:26 +0530

    Intial Commit

---
+++ src/utils/asyncHnadler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((error) => next(error))
+    }
+}
+
+export { asyncHandler }
+
+
+
+
+// Second Method
+// const asyncHandler = (fn) =>  async requestHandler(req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.error(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// } 
